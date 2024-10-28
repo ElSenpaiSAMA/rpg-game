06a6ff84c95d0ff5ed7781c35e224d709db7a941
ElSenpaiSAMA <mnicolas03sp@gmail.com>
2024-10-28 14:05:59 +0100

    Mejorar Movimiento
    establecer diferentes direcciones

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private SpriteRenderer spriteRenderer; 
+    [SerializeField] private Sprite spriteW; 
+    [SerializeField] private Sprite spriteS; 
+    [SerializeField] private Sprite spriteA; 
+    [SerializeField] private Sprite spriteD; 
+
     private PlayerControls playerControls;
     private Vector2 movement;
     private Rigidbody2D rb;
@@ -13,26 +19,57 @@ public class PlayerController : MonoBehaviour
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
-            
     }
+
     private void OnEnable()
     {
         playerControls.Enable();
     }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
     private void Update()
     {
         PlayerInput();
+        ChangeSprite();
     }
+
     private void FixedUpdate()
     {
         Move();
     }
+
     private void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
     }
+
     private void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
     }
+
+    private void ChangeSprite()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            spriteRenderer.sprite = spriteW;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            spriteRenderer.sprite = spriteS;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            spriteRenderer.sprite = spriteA;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            spriteRenderer.sprite = spriteD;
+        }
+    }
 }
+
